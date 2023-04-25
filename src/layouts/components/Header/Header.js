8f9d6b21028ba094c77aa2953bcabd7c75d35a4a
Nguyen Minh Huy <91535034+nhokkoihp01@users.noreply.github.com>
2023-04-25 16:44:57 +0700

    header ++

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -3,96 +3,104 @@ import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import logo from "../../../assets/header/logo-lion.png";
-import { VscBell, VscColorMode, VscQuestion } from "react-icons/vsc";
-import { Button, Image, OverlayTrigger, Tooltip } from "react-bootstrap";
+import {Button, Image, OverlayTrigger, Tooltip} from "react-bootstrap";
+import classNames from "classnames/bind";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+
+import styles from "./Header.module.scss";
+import logo from "~/assets/header/logo-lion.png";
+import {VscBell, VscColorMode, VscQuestion} from "react-icons/vsc";
+
+const cx = classNames.bind(styles);
 
 function NavScrollExample() {
-  return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand
-          href="#"
-          className="navbar-brand-small"
-          style={{ width: "15%" }}
-        >
-          <img
-            src={logo}
-            alt="logo"
-            hight={20}
-            style={{ width: "60%" }}
-            className="d-inline-block align-content-center justify-content-center "
-          />
-        </Navbar.Brand>
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <NavDropdown
-              title="Các Không gian làm việc"
-              id="navbarScrollingDropdown"
-            >
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Gần đây" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Đã đánh dấu sao" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Mẫu" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Button size="lg" type="submit">
-              Tạo mới
-            </Button>{" "}
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              size="lg"
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-          </Form>
+    return (
+        <Navbar className={cx('wrapper')} bg="light" expand="lg">
+            <Container>
+                    <Navbar.Brand
+                        href="#"
+                        className="navbar-brand-small"
+                        style={{width: "15%"}}
+                    >
+                        <img
+                            src={'https://a.trellocdn.com/prgb/assets/d947df93bc055849898e.gif'}
+                            alt="logo"
+                            height={20}
+                            className="d-inline-block align-content-center justify-content-center logo "
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            style={{maxHeight: "100px"}}
+                            navbarScroll
+                        >
+                            <NavDropdown
+                                className={cx('nav-item')}
+                                title="Các Không gian làm việc"
+                                id="navbarScrollingDropdown"
+                            >
+                                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action4">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action5">
+                                    Something else here
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Gần đây" id="navbarScrollingDropdown">
+                                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action4">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action5">
+                                    Something else here
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Đã đánh dấu sao" id="navbarScrollingDropdown">
+                                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action4">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action5">
+                                    Something else here
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Mẫu" id="navbarScrollingDropdown">
+                                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action4">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action5">
+                                    Something else here
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <Button size="lg" type="submit">
+                                Tạo mới
+                            </Button>{" "}
+                        </Nav>
+                        <Form className="d-flex">
+                            <Form.Control
+                                size="lg"
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                        </Form>
 
-          <VscBell size={25} />
-          <VscQuestion size={25} />
-          <VscColorMode size={25} />
-          <Tooltip id="tooltip-top" placement="top" className="tooltip-top">
-            <span>Đổi chế độ nền</span>
-          </Tooltip>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+                        <VscBell size={25}/>
+                        <VscQuestion size={25}/>
+                        <VscColorMode size={25}/>
+                        <Tooltip id="tooltip-top" placement="top" className="tooltip-top">
+                            <span>Đổi chế độ nền</span>
+                        </Tooltip>
+                    </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
 export default NavScrollExample;
