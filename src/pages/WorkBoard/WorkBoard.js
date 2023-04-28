32c31564646ccc1bb669dada5f4113f88a7a54a8
Nguyen Minh Huy <91535034+nhokkoihp01@users.noreply.github.com>
2023-04-28 15:06:44 +0700

    create column api ++

--- src/pages/WorkBoard/WorkBoard.js
+++ src/pages/WorkBoard/WorkBoard.js
@@ -1,45 +1,55 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import classNames from "classnames/bind";
 import {Container, Draggable} from "react-smooth-dnd";
 import {Button, Col, Container as Wrapper, Row} from 'react-bootstrap'
 import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 
 import styles from './WorkBoard.module.scss'
 import Card from "~/pages/WorkBoard/Card";
 import Column from "~/pages/WorkBoard/Column";
 import {applyDrag, generateItems} from "~/untils/DragDrop";
+import {AiOutlinePlus} from 'react-icons/ai'
+import {RiDeleteBack2Fill} from 'react-icons/ri'
+import {createColumn, getAllMemberWorkspace, getWorkspaceById} from "~/services/workspaces.sevices";
 
 const cx = classNames.bind(styles);
 
 function WorkBoard(props) {
-    const [columns, setColumns] = useState({
-        columns: [
-            {
-                id: 'column-1', name: 'Column 1',
-                children: [
-                    {id: 'card-1-1', data: 'Card 1-1'},
-                    {id: 'card-1-2', data: 'Card 1-2'}
-                ]
-            },
-            {
-                id: 'column-2', name: 'Column 2',
-                children: [
-                    {id: 'card-2-1', data: 'Card 2-1'},
-                    {id: 'card-2-2', data: 'Card 2-2'},
-                    {id: 'card-2-3', data: 'Card 2-3'}
-                ]
-            },
-            {
-                id: 'column-3', name: 'Column 3',
-                children: [
-                    {id: 'card-3-1', data: 'Card 3-1'},
-                    {id: 'card-3-2', data: 'Card 3-2'},
-                    {id: 'card-3-3', data: 'Card 3-3'}
-                ]
-            }
-        ]
-    });
+    const [isOpen, setIsOpen] = useState(false)
+    const [columns, setColumns] = useState([])
+    const [columnTitle, setColumnTitle] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+    const handleChangeOpen = () => {
+        setIsOpen(true)
+    }
+    const handleChangeColumnTitle = (e) => {
+        setColumnTitle(e.target.value)
+    }
+    useEffect(() => {
+        setIsLoading(false)
+        async function fetchData() {
+            const response = await getWorkspaceById(1);
+            const data = response?.data;
+            setColumns(data.columns)
+        }
+        fetchData();
+    }, [isLoading]);
+    const handleAddColumns = async () => {
+        const body = {
+            workspaceId: 1,
+            columnName: columnTitle
+        }
+        const response = await createColumn(body)
+        if (response.status === 200) {
+            setIsLoading(true)
+            setIsOpen(false)
+        } else {
+            setIsLoading(false)
+        }
+    }
+
 
     function getCardPayload(columnId, index) {
         return columns.columns.filter(p => p.id === columnId)[0].children[index];
@@ -79,11 +89,38 @@ function WorkBoard(props) {
                         className: 'cards-drop-preview'
                     }}
                 >
-                    {columns.columns.map(column => (
+                    {columns?.map(column => (
                         <Draggable key={column.id}>
                             <Column data={column} onCardDrop={onCardDrop} getCardPayload={getCardPayload}/>
                         </Draggable>
                     ))}
+                    {
+                        !isOpen && (
+                            <div onClick={handleChangeOpen} className={cx('create-column')}>
+                                <AiOutlinePlus/>
+                                <span>Thêm cột</span>
+                            </div>
+                        )
+                    }
+                    {
+                        isOpen && (
+                            <div className={cx('add-column')}>
+                                <div className={cx('input')}>
+                                    <Form.Control
+                                        value={columnTitle}
+                                        onChange={handleChangeColumnTitle}
+                                        size='lg'
+                                        className={cx('input-add')}
+                                        placeholder="Nhập tiêu đề"
+                                    />
+                                </div>
+                                <div className={cx('function')}>
+                                    <Button onClick={() => handleAddColumns()} size="lg" variant="primary">Thêm</Button>
+                                    <RiDeleteBack2Fill onClick={() => setIsOpen(false)} className={cx('icon-remove')}/>
+                                </div>
+                            </div>
+                        )
+                    }
                 </Container>
             </div>
         </div>
