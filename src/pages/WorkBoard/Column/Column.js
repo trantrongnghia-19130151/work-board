aaf6fdaf54026dce0368350f1911469c97f800e6
Nguyen Minh Huy <91535034+nhokkoihp01@users.noreply.github.com>
2023-04-28 15:22:43 +0700

    card ui ++

--- src/pages/WorkBoard/Column/Column.js
+++ src/pages/WorkBoard/Column/Column.js
@@ -1,14 +1,22 @@
 import React, {useState} from 'react';
 import classNames from "classnames/bind";
 import {Container, Draggable} from "react-smooth-dnd";
+import Form from "react-bootstrap/Form";
 
-import styles from "./Column.module.css";
+import styles from "./Column.module.scss";
 import Card from "~/pages/WorkBoard/Card";
+import {Button} from "react-bootstrap";
+import {RiDeleteBack2Fill} from "react-icons/ri";
+import {AiOutlinePlus} from "react-icons/ai";
 
 const cx = classNames.bind(styles);
 
 function Column(props) {
     const {data, getCardPayload, onCardDrop} = props
+    const [isOpen, setIsOpen] = useState(false)
+    const handleChangeOpen = () => {
+        setIsOpen(true)
+    }
 
     return (
         <div className={cx('column')}>
@@ -43,6 +51,31 @@ function Column(props) {
                         <Card card={card}/>
                     </Draggable>
                 ))}
+                {
+                    !isOpen && (
+                        <div onClick={handleChangeOpen} className={cx('create-column')}>
+                            <AiOutlinePlus/>
+                            <span>Thêm card</span>
+                        </div>
+                    )
+                }
+                {
+                    isOpen && (
+                        <div className={cx('add-column')}>
+                            <div className={cx('input')}>
+                                <Form.Control
+                                    size='lg'
+                                    className={cx('input-add')}
+                                    placeholder="Nhập tiêu đề"
+                                />
+                            </div>
+                            <div className={cx('function')}>
+                                <Button  size="lg" variant="primary">Thêm</Button>
+                                <RiDeleteBack2Fill onClick={() => setIsOpen(false)} className={cx('icon-remove')}/>
+                            </div>
+                        </div>
+                    )
+                }
             </Container>
         </div>
     );
