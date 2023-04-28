c05079555cfffdb2869b1a9bc08821ee3f0fb99f
Nguyen Minh Huy <91535034+nhokkoihp01@users.noreply.github.com>
2023-04-28 09:23:43 +0700

    content ui ++

--- src/pages/Home/Content/Content.js
+++ src/pages/Home/Content/Content.js
@@ -1,131 +1,48 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import classNames from "classnames/bind";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {Link} from "react-router-dom";
+
+
 import styles from "./Content.module.scss";
 import hinh7mau from "./hinh7mau.jpg";
+import {BiUser} from 'react-icons/bi'
+import config from "~/config";
+
 const cx = classNames.bind(styles);
 
 function Content(props) {
-    const [showBoardContent, setShowBoardContent] = useState(false);
-    const [showViewContent, setShowViewContent] = useState(false);
-    const [showMemberContent, setShowMemberContent] = useState(false);
-    const [showSettingContent, setShowSettingContent] = useState(false);
-    const [showUpgradeContent, setShowUpgradeContent] = useState(false);
-    return ( <
-        div >
-        <
-        div >
-        <
-        h3 > Bảng đã xem < /h3>
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('board-box')}>
+                <h3 className={cx('header')}><BiUser/>
+                    Your boards
+                </h3>
+                <Container>
+                    <Row>
+                        <Col lg={3} md={3}>
+                            <Link to={config.routes.createWorkspace}>
+                                <div className={cx('board-item')}>
+                                    <img src={hinh7mau} alt=""/>
+                                    <p className={cx('text')}>Test</p>
+                                </div>
+                            </Link>
+                        </Col>
+                        <Col lg={3} md={3}>
+                            <Link to={config.routes.createWorkspace}>
+                                <div className={cx('create-board')}>
+                                    <p className={cx('create-text')}>Create new Board</p>
+                                </div>
+                            </Link>
+                        </Col>
+                    </Row>
 
-        <ul style={{ display: 'flex', listStyle: 'none', padding: 0 }}>
-        <li style={{ display: 'inline-block', marginRight: '20px' }}>Board 1 <img src={hinh7mau} style={{ width: '200px', height: 'auto', border: '1px solid #ccc', borderRadius: '5px', boxShadow: '2px 2px 5px #ccc', marginLeft: '10px' }}/></li>
-        <li style={{ display: 'inline-block', marginRight: '20px' }}>Board 2 <img src={hinh7mau} style={{ width: '200px', height: 'auto', border: '1px solid #ccc', borderRadius: '5px', boxShadow: '2px 2px 5px #ccc', marginLeft: '10px' }}/></li>
-        <li style={{ display: 'inline-block', marginRight: '20px' }}>Board 3 <img src={hinh7mau} style={{ width: '200px', height: 'auto', border: '1px solid #ccc', borderRadius: '5px', boxShadow: '2px 2px 5px #ccc', marginLeft: '10px' }}/></li>
-        </ul> <
-        /div> <
-        div >
-        <
-        h3 > CÁC KHÔNG GIAN LÀM VIỆC CỦA BẠN < /h3>
-            <ul style={{ display: 'flex', listStyle: 'none', padding: 0 }}>
-                <li style={{ display: 'inline-block', marginRight: '20px' }}>Board 1 <img src={hinh7mau} style={{ width: '200px', height: 'auto', border: '1px solid #ccc', borderRadius: '5px', boxShadow: '2px 2px 5px #ccc', marginLeft: '10px' }}/></li>
-                <li style={{ display: 'inline-block', marginRight: '20px' }}>Board 2 <img src={hinh7mau} style={{ width: '200px', height: 'auto', border: '1px solid #ccc', borderRadius: '5px', boxShadow: '2px 2px 5px #ccc', marginLeft: '10px' }}/></li>
-                <li style={{ display: 'inline-block', marginRight: '20px' }}>Board 3 <img src={hinh7mau} style={{ width: '200px', height: 'auto', border: '1px solid #ccc', borderRadius: '5px', boxShadow: '2px 2px 5px #ccc', marginLeft: '10px' }}/></li>
-            </ul><
-        div className = "workspace-sections"
-        style = {
-            { display: 'flex', gap: '16px' } } >
-        <
-        h4 > Tên không gian < /h4> <
-        div >
-        <
-        button onClick = {
-            () => setShowBoardContent(!showBoardContent) } > Bảng < /button> {
-            showBoardContent &&
-                <
-                div >
-                <
-                ul >
-                <
-                li > Board 1 < /li> <
-                li > Board 2 < /li> <
-                li > Board 3 < /li> <
-                /ul> <
-                /div>
-        } <
-        /div> <
-        div >
-        <
-        button onClick = {
-            () => setShowViewContent(!showViewContent) } > Dạng xem < /button> {
-            showViewContent &&
-                <
-                div >
-                <
-                ul >
-                <
-                li > Dạng xem 1 < /li> <
-                li > Dạng xem 2 < /li> <
-                li > Dạng xem 3 < /li> <
-                /ul> <
-                /div>
-        } <
-        /div> <
-        div >
-        <
-        button onClick = {
-            () => setShowMemberContent(!showMemberContent) } > Thành viên < /button> {
-            showMemberContent &&
-                <
-                div >
-                <
-                p > Danh sách các thành viên: < /p> <
-                ul >
-                <
-                li > Thành viên 1 < /li> <
-                li > Thành viên 2 < /li> <
-                li > Thành viên 3 < /li> <
-                /ul> <
-                /div>
-        } <
-        /div> <
-        div >
-        <
-        button onClick = {
-            () => setShowSettingContent(!showSettingContent) } > Cài đặt < /button> {
-            showSettingContent &&
-                <
-                div >
-                <
-                ul >
-                <
-                li > Cập nhập < /li> <
-                li > Sửa < /li> <
-                /ul> <
-                /div>
-        } <
-        /div> <
-        div >
-        <
-        button onClick = {
-            () => setShowUpgradeContent(!showUpgradeContent) } > Nâng cấp < /button> {
-            showUpgradeContent &&
-                <
-                div >
-                <
-                ul >
-                <
-                li > Thẻ Basic < /li> <
-                li > Thẻ Premium < /li> <
-                li > Thẻ Enterprise < /li> <
-                /ul> <
-                /div>
-        } <
-        /div> <
-        /div> <
-        /div> <
-        /div>
-
-    );
+                </Container>
+            </div>
+        </div>
+    )
 }
 
 
