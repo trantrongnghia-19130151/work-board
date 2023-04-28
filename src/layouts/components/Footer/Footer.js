071baed81162dda2cb2e9333a3720edd6cf213ab
NguyenNgocHieuu <19130072@st.hcmuaf.edu.vn>
2023-04-28 07:43:20 +0700

    footer

--- src/layouts/components/Footer/Footer.js
+++ src/layouts/components/Footer/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import classNames from "classnames/bind";
 
+import { Container, Row, Col } from 'react-bootstrap';
+import { FaFacebookSquare, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 import styles from './Footer.module.scss';
 
@@ -9,41 +11,41 @@ const cx = classNames.bind(styles);
 function Footer(props) {
     return (
         <div className={cx('wrapper')}>
-            <div className="mt-5 pt-5 pb-5 footer">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-5 col-xs-12 about-company">
-                            <h2>Heading</h2>
-                            <p className="pr-5 text-white-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Nullam ac ante mollis quam tristique convallis </p>
-                            <p><a href="#"><i className="fa fa-facebook-square mr-1"></i></a><a href="#"><i
-                                className="fa fa-linkedin-square"></i></a></p>
-                        </div>
-                        <div className="col-lg-3 col-xs-12 links">
-                            <h4 className="mt-lg-0 mt-sm-3">Links</h4>
-                            <ul className="m-0 p-0">
-                                <li>- <a href="#">Lorem ipsum</a></li>
-                                <li>- <a href="#">Nam mauris velit</a></li>
-                                <li>- <a href="#">Etiam vitae mauris</a></li>
-                                <li>- <a href="#">Fusce scelerisque</a></li>
-                                <li>- <a href="#">Sed faucibus</a></li>
-                                <li>- <a href="#">Mauris efficitur nulla</a></li>
+            <footer className="bg-dark text-light">
+                <Container className="py-5">
+                    <Row>
+                        <Col lg={6}>
+                            <h3>Lion Home Service</h3>
+                            <p className="text-secondary">Công ty của chúng tôi là một trong những công ty hàng đầu trong lĩnh vực của mình. Với đội ngũ nhân viên giàu kinh nghiệm và tâm huyết, chúng tôi cam kết mang đến cho khách hàng những sản phẩm và dịch vụ chất lượng nhất. </p>
+                            <ul className="list-inline mt-4">
+                                <li className="list-inline-item"><a href="#"><FaFacebookSquare /></a></li>
+                                <li className="list-inline-item"><a href="#"><FaTwitter /></a></li>
+                                <li className="list-inline-item"><a href="#"><FaInstagram /></a></li>
                             </ul>
-                        </div>
-                        <div className="col-lg-4 col-xs-12 location">
-                            <h4 className="mt-lg-0 mt-sm-4">Location</h4>
-                            <p>22, Lorem ipsum dolor, consectetur adipiscing</p>
-                            <p className="mb-0"><i className="fa fa-phone mr-3"></i>(541) 754-3010</p>
-                            <p><i className="fa fa-envelope-o mr-3"></i>info@hsdf.com</p>
-                        </div>
-                    </div>
-                    <div className="row mt-5">
-                        <div className="col copyright">
-                            <p className=""><small className="text-white-50">© 2019. All Rights Reserved.</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                        </Col>
+                        <Col lg={3} md={6} className="mt-4 mt-lg-0">
+                            <h5>Links</h5>
+                            <ul className="list-unstyled">
+                                <li><a href="#" className="text-secondary">Lorem ipsum</a></li>
+                                <li><a href="#" className="text-secondary">Dolor sit amet</a></li>
+                                <li><a href="#" className="text-secondary">Consectetur</a></li>
+                                <li><a href="#" className="text-secondary">Adipiscing elit</a></li>
+                            </ul>
+                        </Col>
+                        <Col lg={3} md={6} className="mt-4 mt-lg-0">
+                            <h5>Contact Us</h5>
+                            <ul className="list-unstyled">
+                                <li><a href="#" className="text-secondary">Khu phố 6, phường Linh Trung, TP Thủ Đức, TP HCM</a></li>
+
+                                <li><a href="#" className="text-secondary">lion.team@gmail.com</a></li>
+                                <li><a href="#" className="text-secondary">+1 (555) 555-5555</a></li>
+                            </ul>
+                        </Col>
+                    </Row>
+                    <hr className="my-5" />
+                    <p className="text-secondary text-center">&copy; 2023 Lion Home Service. All rights reserved.</p>
+                </Container>
+            </footer>
         </div>
     );
 }
